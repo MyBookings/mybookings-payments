cd0df29cd1809bda5a3f3d38f00d6468d0645bdf
my-bookings <labaas@gmail>
2021-11-16 16:25:21 +0100

    update

--- src/server.js
+++ src/server.js
@@ -1,30 +1,16 @@
-import WebSocket from 'ws';
-import { serialize } from 'cookie';
+import { WebSocketServer } from 'ws';
 
-const origin = 'https://mybookings-payments.herokuapp.com';
+const PORT = process.env.PORT || 8000;
 
-const cookie = serialize('Cookie', 'E0D439EE522F44368DC78E1BFB03710C-D24FB11DBE31D4621C4817E028D9E1D; AccessToken=C66EF7B239D24632943D115EDE9CB810-EA00F8FD8294692C940F6B5A8F9453D');
+const wss = new WebSocketServer({ port: PORT });
 
-const ws = new WebSocket('wss://ws.mews-demo.com',
-  [], 
-  {
-    'headers': {
-      'Cookie': cookie,
-    }
-  }
-);
+wss.on('connection', (ws) => {
 
-ws.on('open', (connection) => {
-  console.log(connection);
-  ws.send('hello');
-});
+  ws.on('message', (message) => {
+    console.log(message);
+  });
 
-ws.on('error', (error) => {
-  console.log(error);
-});
-
-ws.on('message', (message) => {
-  console.log(message);
+  ws.send('Welcome!');
 });
 
 
