b3065373eaf35c68e277654eb0a9a443b9de06b3
my-bookings <labaas@gmail>
2021-11-16 16:45:47 +0100

    update

--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
-import { WebSocketServer } from 'ws';
+import { Server } from 'ws';
+import express from 'express';
 
-const wss = new WebSocketServer({ port: 8000 });
+const server = express();
+const PORT = process.env.PORT || 8000;
+
+server.use((req, res) => {
+  res.send('OK!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
+
+const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
 
   ws.on('message', (message) => {
     console.log(message);
   });
+
+  ws.on('close', () => console.log('Client disconnected'));
 
   ws.send('Welcome!');
 });
