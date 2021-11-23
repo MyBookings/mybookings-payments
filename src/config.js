e583f69c082b4b45f292c7acd032be351e8a3204
my-bookings <labaas@gmail>
2021-11-23 15:39:27 +0100

    update

--- src/config.js
+++ src/config.js
@@ -3,3 +3,7 @@ export const CLIENT_TOKEN = 'E0D439EE522F44368DC78E1BFB03710C-D24FB11DBE31D4621C
 export const ACCESS_TOKEN = 'C66EF7B239D24632943D115EDE9CB810-EA00F8FD8294692C940F6B5A8F9453D';
 export const ORIGIN = 'https://mybookings-payments.herokuapp.com';
 export const MAX_AGE = 60 * 60 * 8 // 8 hours
+
+export const url = process.env.ENV === 'production'
+  ? 'wss://mybookings-payments.herokuapp.com'
+  : 'ws://localhost:8000'
--- src/server.js
+++ src/server.js
@@ -1,12 +1,117 @@
-import { WebSocketServer } from 'ws';
+require('dotenv').config();
+import WebSocket from 'ws';
+import express from 'express';
+import axios from 'axios';
+import moment from 'moment';
+// import createMollieClient from '@mollie/api-client';
 
-const wss = new WebSocketServer({ port: process.env.PORT || 8000 });
+const CLIENT_TOKEN = process.env.CLIENT_TOKEN;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const MOLLIE_KEY = process.env.MOLLIE_KEY;
 
-wss.on('connection', (ws) => {
+const WEBSOCKET_ADRESS = 'wss://ws.mews-demo.com/ws/connector';
+const GET_ALL_RESERVATIONS_URL = 'https://api.mews-demo.com/api/connector/v1/reservations/getAll';
+const GET_PRICE_RESERVATIONS_URL = 'https://api.mews-demo.com/api/connector/v1/reservations/price';
 
-  ws.on('message', (message) => {
-    console.log(JSON.parse(message));
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.get('/', (req, res) => res.status(200).send('OK'));
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+  
+const ws = new WebSocket(
+  WEBSOCKET_ADRESS,
+  [],
+  {
+    'headers': {
+      'Cookie': `ClientToken=${CLIENT_TOKEN}; AccessToken=${ACCESS_TOKEN}`
+    }
+  }
+);
+
+ws.on('open', () => {
+  console.log('connected');
+});
+
+ws.on('message', async (message) => {
+  
+  const parsed = JSON.parse(message);
+  console.log(parsed);
+  const events = parsed.Events?.map(event => event);
+
+  const reservationEvents = events.every(event => event.Type === 'Reservation');
+  if (!events || !reservationEvents) return;
+
+  const confirmedEvents = events.filter(event => event.State === 'Confirmed');
+  if (!confirmedEvents[0]) return; 
+
+  const eventIds = confirmedEvents.map(event => event.Id);
+
+  const { data: { Reservations, Rates } } = await axios({
+    method: 'post',
+    url: GET_ALL_RESERVATIONS_URL,
+    data: {
+      ClientToken: CLIENT_TOKEN,
+      AccessToken: ACCESS_TOKEN,
+      Client: 'Sample Client 1.0.0',
+      ReservationIds: [...eventIds],
+      Currency: 'EUR',
+      Extent: {
+        Reservations: true,
+        Rates: true,
+      }
+    },
   });
 
-  ws.send(JSON.stringify('WELCOME!!!'));
+  const newReservations = Reservations.every(event => {
+    let created = moment(event.CreatedUtc);
+    let updated = moment(event.UpdatedUtc);
+    let difference = updated.diff(created, 'seconds');
+    if (difference > 2) return false;
+    return true;
+  });
+
+  if (!newReservations) return;
+
+  const rateIds = Reservations.map(
+    (reservation) => reservation.RateId
+  );
+
+  const rates = rateIds.map(rateId => {
+    return Rates.find(rate => {
+      return rate.Id === rateId;
+    });
+  });
+
+  const serviceId = [...new Set(rates.map(rate => rate.ServiceId))][0];
+
+  const { data: { ReservationPrices } } = await axios({
+    method: 'post',
+    url: GET_PRICE_RESERVATIONS_URL,
+    data: {
+      ClientToken: CLIENT_TOKEN,
+      AccessToken: ACCESS_TOKEN,
+      Client: 'Sample Client 1.0.0',
+      ServiceId: serviceId,
+      Reservations: [...Reservations],
+    },
+  });
+
+  console.log(ReservationPrices[0].Total.Value);
+
+  
+
+  // const mollieClient = await createMollieClient({
+  //   apiKey: MOLLIE_KEY,
+  // });
+
 });
+
+ws.on('close', () => {
+  console.log('disconnected');
+});
+
+ws.on('error', (error) => {
+  console.log(error);
+});
+
